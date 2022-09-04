9abed845ec68f71875fd54ae6363a9d985c1484d
Jeff-w939 <60969920+Jeff-w939@users.noreply.github.com>
2022-09-04 14:11:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oAouR6ieFy6FFR_lOS0vEXZDP_24",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "zA329xvShprfifrmOUm1pRLAegj7VGaIjcsOhlSWeTY",
+      useTemplateId: "a1iA4f-7rfmvzPAGOJ1CFYa92eo57U0DtR-_a3Xb4pQ",
       // 所在省份
       province: "贵州",
       // 所在城市
@@ -42,7 +42,7 @@ export const config = {
     {
       name: "珊珊", 
       id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
-      useTemplateId: "zA329xvShprfifrmOUm1pRLAegj7VGaIjcsOhlSWeTY",
+      useTemplateId: "a1iA4f-7rfmvzPAGOJ1CFYa92eo57U0DtR-_a3Xb4pQ",
       province: "贵州",
       city: "贵阳",
       openUrl: "https://zhangshanshan.netlify.app/"
